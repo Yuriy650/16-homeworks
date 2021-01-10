84a838587a9d6658083b683179c6794abc8fc6bc
Yuriy <leorentex@gmail.com>
2021-01-10 02:58:45 +0200

    deploy react app to github pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Contracts from "./Contracts/Contracts";
 
-function App(props) {
-  return (
-    <div className="App">
-      <Contracts />
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <Contracts/>
+        </div>
+    );
 }
 
 export default App;
